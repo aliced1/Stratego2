5698692ed1d7a66ee114181ffbd9da09b4ed789a
alice <alice.duff5@gmail.com>
2023-03-04 15:25:50 -0500

    Working on cursor and user input

--- core/src/com/stratego/game/StrategoGame.java
+++ core/src/com/stratego/game/StrategoGame.java
@@ -1,6 +1,8 @@
 package com.stratego.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,13 +12,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class StrategoGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
 	private OrthographicCamera camera;
-	Sprite gridSquare;
-	Sprite gridLake;
+	private Sprite gridSquare;
+	private Sprite gridLake;
 	private int squaresOnWidth;
 	private int squaresOnHeight;
+
+	private Sprite cursor;
+
 
 	@Override
 	public void create () {
@@ -24,6 +29,11 @@ public class StrategoGame extends ApplicationAdapter {
 		img = new Texture("red.png");
 		gridSquare = new Sprite(new Texture("white.png"));
 		gridLake = new Sprite(new Texture("black.png"));
+
+		cursor = new Sprite(new Texture("Spy.png"));
+		cursor.setScale(0.1f);
+		cursor.setPosition(0, 0);
+
 
 //		Gdx.graphics.setWindowedMode(800, 800);
 
@@ -42,6 +52,17 @@ public class StrategoGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 1, 1, 1);
+
+		// move cursor according to input
+		int movementScale = 10;
+		if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) cursor.translateX(-movementScale);
+		if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) cursor.translateX(movementScale);
+		if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) cursor.translateY(-movementScale);
+		if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) cursor.translateY(movementScale);
+
+		Gdx.gl.glClearColor(0.8f, 0.8f, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		float squareWidth = camera.viewportWidth / squaresOnWidth;
 		float squareHeight = camera.viewportHeight / squaresOnHeight;
 		gridSquare.setSize(squareWidth, squareHeight);
@@ -62,13 +83,16 @@ public class StrategoGame extends ApplicationAdapter {
 				gridSquare.draw(batch);
 			}
 		}
+
+		cursor.draw(batch);
+
 		batch.end();
 
 
 
 
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
